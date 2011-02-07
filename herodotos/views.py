12a7b6fa58e4763e505ab9f8ca51855549ecc736
Baptiste Mispelon <bmispelon@gmail.com>
2011-02-07 22:17:57 +0100

    Added a class-based view mixin.

--- herodotos/views.py
+++ herodotos/views.py
@@ -2,6 +2,15 @@ from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404
 from django.views.generic.list_detail import object_list
 
+class HerodotosEnabledMixin(object):
+    action_id = None
+    
+    def record(self, comment='', user=None):
+        if user is None:
+            user = self.request.user
+        if self.action_id is not None:
+            self.object.record(action=self.action_id, comment=comment, user=user)
+
 def object_history(request, app_label, model, object_pk):
     contenttype = get_object_or_404(ContentType, app_label=app_label, model=model)
     object = get_object_or_404(contenttype.model_class(), pk=object_pk)
