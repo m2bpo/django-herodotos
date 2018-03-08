91328df390e4e696a812c61231fda9fb691a76ec
Baptiste Mispelon <bmispelon@gmail.com>
2018-03-08 13:03:42 +0100

    Added custom queryset/manager for Event

--- herodotos/models.py
+++ herodotos/models.py
@@ -1,11 +1,21 @@
+import isoweek
+
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
+
 from herodotos.actions import get_action_list_for_contenttype
 from toolbox.choices import pick_choice
+
+
+class EventQuerySet(models.QuerySet):
+    def for_week(self, *args, **kwargs):
+        w = isoweek.Week(*args, **kwargs)
+        start, end = w.monday(), w.sunday()
+        return self.filter(date__date__range=(start, end))
 
 
 class Event(models.Model):
@@ -18,6 +28,8 @@ class Event(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+    
+    objects = EventQuerySet.as_manager()
     
     class Meta:
         verbose_name = _('event')
